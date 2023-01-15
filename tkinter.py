c5257963ad6bf30f382c42570ef6135bc3903880
AllRightG <sge31@yandex.ru>
2023-01-15 15:30:54 +0300

    Создание окошка нормального

--- tkinter.py
+++ tkinter.py
@@ -1,3 +1,25 @@
 from tkinter import *
 
 window = Tk()
+
+window.title('TEST')
+window.geometry('200x160')
+
+label = Label(
+  text = 'Hello!',
+  width = 30,
+  height = 5,
+  underline = 0
+)
+
+button = Button(
+  text = 'Click',
+  width = 30,
+  height = 5,
+  activebackground = 'red'
+)
+
+label.pack()
+button.pack()
+
+window.mainloop()
